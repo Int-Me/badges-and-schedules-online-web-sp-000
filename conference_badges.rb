f4ce25980f51f861b17e5efaed8abf3bffebb9e2
Roland McRae <mcraeroland@gmail.com>
2020-04-08 12:06:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,6 +18,13 @@ def assign_rooms(array)
   room_messages
 end
 
-def printer
-
+def printer(array)
+  badge_messages = batch_badge_creator(speakers)
+  badge_messages.each do |message|
+    puts message
+  end
+  room_messages = assign_rooms(speakers)
+  room_messages.each do |message|
+    puts message
+  end
 end
