e18ab3a1359987a3b8bf6510dbec736143694551
Roland McRae <mcraeroland@gmail.com>
2020-04-08 12:03:47 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(array)
 }
   badge_messages
 end
+
+def assign_rooms(array)
+  badge_messages = []
+  array.each_with_index { |item, index|
+  badge_messages[index] = "Hello, my name is #{item}."
+}
+  badge_messages
+end
